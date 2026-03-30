571f00387a20474fce60534e28963c7cd796f1a6
tangweigang-jpg <259277235+tangweigang-jpg@users.noreply.github.com>
2026-03-31 00:55:35 +0700

    chore: package self-contained skill for release

--- skills/doramagic/packages/doramagic_product/doramagic_product/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Doramagic product pipeline."""
-
-from .pipeline import CandidateInfo, DoramagicProductPipeline, DoramagicRunResult
-
-__all__ = ["CandidateInfo", "DoramagicProductPipeline", "DoramagicRunResult"]
